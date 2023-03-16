053d873d4317f7888901f4d94da8cae7dab57923
unknown <mosolova.viktoriya@mail.ru>
2023-03-16 17:49:53 +0300

    макет блога в html+css

---
+++ web/09.03.23/script.js
@@ -0,0 +1,13 @@
+//создать рандомный массив
+//вывести его в консоль
+//сложить все числа 
+//вывести в консоль
+
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+console.log(typeof arr)
+let sum = 0
+for (let i = 0; i < arr.length; i++) {
+    sum += arr[i]
+}
+console.log(typeof sum)
